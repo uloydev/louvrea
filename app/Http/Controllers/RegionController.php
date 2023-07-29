ed18f652e3ab0ade5535ffa5c90bea5635f4a107
Wahyu Miftahul Aflah <wahyu.miftahul@dansmultipro.com>
2023-07-29 14:13:30 +0700

    fix: regency data

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -8,19 +8,9 @@ use App\Models\Regency;
 
 class RegionController extends Controller
 {
-    public function provinces()
-    {
-        return Province::all();
-    }
-
     public function jabodetabekRegency()
     {
         return Regency::whereIn('id', Regency::jabodetabek)->get();
-    }
-
-    public function regencyByProvince(int $id)
-    {
-        return Regency::where('province_id', $id)->get();
     }
 
     public function districtByRegency(int $id)
--- app/Models/Regency.php
+++ app/Models/Regency.php
@@ -9,7 +9,7 @@ class Regency extends Model
 {
     use HasFactory;
 
-    public const jabodetabek = [3201,3202,3276,3171,3172,3173,3174,3175,3216,3275,3603,3671,3674];
+    public const jabodetabek = [3201,3276,3171,3172,3173,3174,3175,3216,3275,3603,3671,3674];
 
     protected $table = "reg_regencies";
 }
