20ddba6ba9064cea7c6c4aef62f119da95dbd116
Wahyu Miftahul Aflah <wahyu.miftahul@dansmultipro.com>
2023-08-19 00:29:33 +0700

    fix: remove images from about page

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -10,20 +10,5 @@
             Lip Serum Louvrea, Eye Cream Louvrea hingga produk makeup seperti Makeup Louvrea, Lip Cream Louvrea.
             Louvrea juga menyediakan berbagai macam produk untuk pria seperti,
             Cream Wajah Louvrea, Facial Wash, hingga Toner. Harga yang ditawarkan juga relatif terjangkau.</p>
-
-        <div class="row justify-content-center">
-            <div class="col-sm-3">
-                <img src="{{ asset('img/dailyglow.jpg') }}" alt="Gambar 1" class="img-fluid">
-            </div>
-            <div class="col-sm-3">
-                <img src="{{ asset('img/lipposerum.jpg') }}" alt="Gambar 2" class="img-fluid">
-            </div>
-            <div class="col-sm-3">
-                <img src="{{ asset('img/toner.jpg') }}" alt="Gambar 3" class="img-fluid">
-            </div>
-            <div class="col-sm-3">
-                <img src="{{ asset('img/facialwash.jpg') }}" alt="Gambar 4" class="img-fluid">
-            </div>
-        </div>
     </div>
 @endsection
